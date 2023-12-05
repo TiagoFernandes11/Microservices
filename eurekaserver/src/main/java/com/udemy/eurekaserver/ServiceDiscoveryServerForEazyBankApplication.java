23f03612b7b478054688187d27e35d9f638fc7a7
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-12-05 13:37:13 -0300

    Eureka server (Service Descovery) creation

---
+++ eurekaserver/src/main/java/com/udemy/eurekaserver/ServiceDiscoveryServerForEazyBankApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryServerForEazyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryServerForEazyBankApplication.class, args);
+	}
+
+}
